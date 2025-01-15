5111a86f5760dc1cd1557aa125337578b6d93a57
sunny <parksunsunny@gmail.com>
2025-01-15 17:52:15 +0900

    main page update 롤백

--- dangun/src/main/java/com/example/dangun/Controller/TestController.java
+++ dangun/src/main/java/com/example/dangun/Controller/TestController.java
@@ -1,13 +1,14 @@
 package com.example.dangun.Controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
-
-    @GetMapping("/")
-    public String showHelloPage() {
-        return "main";
-    }
+	
+	@GetMapping("/")
+	public ResponseEntity Test() {
+		return ResponseEntity.ok("hi");
+	}
 }
