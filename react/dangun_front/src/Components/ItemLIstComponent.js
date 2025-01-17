21f1fbef1e499fede11930bbaf35817daac1ea0b
sunny <parksunsunny@gmail.com>
2025-01-17 17:22:31 +0900

    - ItemList 화면 수정

--- react/dangun_front/src/Components/ItemLIstComponent.js
+++ react/dangun_front/src/Components/ItemLIstComponent.js
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import {timeCulFunc} from "../utils/timeUtil";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
+import "./ItemListComponent.css";
+
 const ItemListComponent = ( ) => {
 
     const [itemList, setItemList] = useState([]);
@@ -15,31 +19,46 @@ const ItemListComponent = ( ) => {
 
     },[]);
 
-    return (
-        <div>
-            <table>
-                <tr>
-                    <th>품명</th><th>가격</th><th>지역</th><th>카테고리</th>
-                </tr>
-                {
-                    itemList && itemList.length > 0 ? (
-                        itemList.map(item => (
-                            <tr key={item.id}>
-                                <td><a href={`/item/detail/${item.id}`}>{item.title}</a></td>
-                                <td>{item.price}</td>
-                                <td>{item.country}</td>
-                                <td>{item.category}</td>
-                                <td>{timeCulFunc(item.writeDate)}</td>
-                            </tr>
-                        ))
-                    ) : (
-                        <tr>
-                            <td colSpan="4">아이템이 없습니다.</td>
-                        </tr>
-                    )
-                }
-            </table>
+    return (        
+        <>
+        {/* Header  */}
+        <div className="header">
+        <Header />
         </div>
+        <div className="box_container">
+            <div className="box_item box_list">
+                <div>
+                    <table>
+                        <tr>
+                            <th>품명</th><th>가격</th><th>지역</th><th>카테고리</th>
+                        </tr>
+                        {
+                            itemList && itemList.length > 0 ? (
+                                itemList.map(item => (
+                                    <tr key={item.id}>
+                                        <td><a href={`/item/detail/${item.id}`}>{item.title}</a></td>
+                                        <td>{item.price}</td>
+                                        <td>{item.country}</td>
+                                        <td>{item.category}</td>
+                                        <td>{timeCulFunc(item.writeDate)}</td>
+                                    </tr>
+                                ))
+                            ) : (
+                                <tr>
+                                    <td colSpan="4">아이템이 없습니다.</td>
+                                </tr>
+                            )
+                        }
+                    </table>
+                </div>
+            </div>
+        </div>
+
+        {/* Footer */}
+      <div className="footer">
+        <Footer />        
+      </div>
+    </>
     )
 }
 
